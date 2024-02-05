1b4a227d2609cd327525b50b6251adf1c4c5428f
Tirath Bhathawala <tirath.bhathawala@gmail.com>
2024-02-05 22:14:17 +0530

    Taking gridSize from user and optimize script.js code

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const gridSize = 20
+let gridSize = parseInt(prompt("Enter grid size: "));
 
-let gridExternal = document.querySelector("#grid")
+while (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+    gridSize = parseInt(prompt("Please enter a valid grid size (a positive number lesser than 100): "));
+}
+
+const gridExternal = document.querySelector("#grid")
+const gridInternalItems = []
+let hoverColor = "violet"
 gridExternal.classList.add("grid-external")
 
 for (let i = 0; i < gridSize; i++)
@@ -13,20 +19,17 @@ for (let i = 0; i < gridSize; i++)
         const gridInternal = document.createElement("div");
         gridInternal.classList.add("grid-internal")
         row.appendChild(gridInternal)
+        gridInternalItems.push(gridInternal)
     }
     gridExternal.appendChild(row)
 }
 
 gridExternal.style.setProperty("--grid-size", gridSize)
 
-const gridInternalItems = document.querySelectorAll(".grid-internal")
-
-gridInternalItems.forEach(gridInternal => {
-    gridInternal.addEventListener("mouseover", event => {
-        event.target.classList.add('hovered');
-        event.target.style.backgroundColor = hoverColor;
-    })
-})
+const colorPicker = document.querySelector("#colorPicker");
+colorPicker.addEventListener("input", () => {
+    hoverColor = colorPicker.value; 
+});
 
 const resetButton = document.querySelector("#resetButton");
 resetButton.addEventListener("click", () => {
@@ -35,8 +38,15 @@ resetButton.addEventListener("click", () => {
     });
 });
 
-let hoverColor = "violet";
-const colorPicker = document.querySelector("#colorPicker");
-colorPicker.addEventListener("input", () => {
-    hoverColor = colorPicker.value; 
+gridExternal.addEventListener("mouseover", event => {
+    if (event.target.classList.contains('grid-internal')) {
+        event.target.classList.add('hovered');
+        event.target.style.backgroundColor = hoverColor;
+    }
+});
+
+gridExternal.addEventListener("mouseout", event => {
+    if (event.target.classList.contains('grid-internal')) {
+        event.target.classList.remove('hovered');
+    }
 });
